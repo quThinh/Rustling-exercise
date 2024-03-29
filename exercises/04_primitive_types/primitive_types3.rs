9d8d5b2704ece9f03dfdcab2a8a7881089ffcb5d
Sotatek-ThinhTruong <thinh.truong@sotatek.com>
2024-03-29 15:20:00 +0700

    exercise

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,10 +5,8 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = "The sun dipped below the horizon, casting hues of orange and pink across the sky. The gentle breeze rustled the leaves, creating a soothing melody. As the day bid farewell, a sense of tranquility enveloped the world, promising a peaceful night ahead.";
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
